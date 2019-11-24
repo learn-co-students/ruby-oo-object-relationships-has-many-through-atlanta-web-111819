4be14cf02d0b3f184bb2c9fbb12e166af6147817
Javi <javsteratii@gmail.com>
2019-11-24 14:04:37 -0500

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,24 @@
 class Meal
+    #joining table for waiter and meals
+
+    #needs to now everything about the meals 
+    #including the customer,waiter, tip, cost, etc
+    attr_accessor :waiter, :customer, :total, :tip
+    @@all = []
+
+    # how does this joiner table have access to the 
+    # customer and waiter??? IDEKKKKK
+
+    def initialize(waiter, customer, total, tip=0)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all 
+    end 
 
 end
