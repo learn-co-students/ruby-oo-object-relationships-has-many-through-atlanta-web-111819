916cf6563356383cde375397e3f9068d72149858
github username <github email address>
2019-11-27 22:16:43 -0500

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
 class Customer
-  
+    
+    attr_reader :name, :age
+    @@all = []
+
+    def initialize(name, age)
+      @age = age
+      @name = name
+      @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.customer == self
+        end
+    end
+
+    def waiters
+      meals = Meal.all.select do |meal|
+        meal.customer == self
+      end
+      meals.map do |meal|
+        meal.waiter
+      end
+    end
 end
